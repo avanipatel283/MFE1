747c3966949c781ac24dca9c2b0c0717fdd852d2
avanipatel283 <avani.kumar@anywhere.co>
2023-11-24 16:41:41 +0530

    MicroFront End 1 Application

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Button from './Button';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>MFE1</h1>
+      <Button>
+        {''}
+        MFE1 Button
+      </Button>
+    </div>
+  );
+}
+
+export default App;
